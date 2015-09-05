c93e1a2d7dd36aa382dd1dd482bcc03313e48eda
Daniel Beeke <daniel@studiofonkel.nl>
2015-09-05 11:16:13 +0200

    Initial commit

---
+++ router.js
@@ -0,0 +1,27 @@
+Router.route(
+    '/',
+    {
+        name: 'home'
+    }
+);
+
+Router.route(
+    '/manage',
+    {
+        name: 'manage'
+    }
+);
+
+Router.route(
+    '/manage/questions',
+    {
+        name: 'manage-questions'
+    }
+);
+
+Router.route(
+    '/questionnaire',
+    {
+        name: 'questionnaire'
+    }
+);
